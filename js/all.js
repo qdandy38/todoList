6b8d5c86f145d7e009e112d7583be17300702a7c
YunHsuan Tsai <qdandy38@gmail.com>
2020-06-20 17:31:53 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,56 +1,86 @@
-//指定DOM
-var list = document.querySelector('.list');
-var sendData = document.getElementById('send');
-var data = JSON.parse(localStorage.getItem('listData')) || []; //抓出localStorage內的資料並轉為陣列，如果沒有則回傳空陣列
+// 指定DOM
+const send = document.getElementById('send');
+const reStart = document.getElementById('reStart');
+const list = document.querySelector('.list');
+const content = document.querySelector('.content');
+const feedback = document.querySelector('.invalid-feedback');
 
-//監聽
-sendData.addEventListener('click', addData, false);
-list.addEventListener('click', toggleDone, false);
-updateList(data); //一開始先更新網頁內容，顯示的是目前localStorage內的資料
+let data = JSON.parse(localStorage.getItem('todoList')) || []; // 如果localStorage有資料則載入，否則預設為空
 
-//function
+// 監聽
+send.addEventListener('click', addData, false);
+list.addEventListener('click', updateData, false);
+reStart.addEventListener('click', clearAll, false);
+window.addEventListener('load', init, false);
+
 // 按下enter=按下送出鈕
 window.addEventListener('keyup', function(e) {
     if (e.keyCode === 13) {
-        sendData.click();
+        send.click();
     }
 }, false);
-//新增待辦事項
+
+// function
+function init() {
+    render(); // 一開始先渲染一次畫面
+};
+
+// 增加資料到data[]並存入localStorage
 function addData(e) {
     e.preventDefault();
-    var txt = document.querySelector('.search').value; //抓出input的內容
-    if (txt !== '') { //有輸入內容才傳值
-        var todo = { //儲存txt的Obj
-            content: txt
+    feedback.setAttribute('style', 'display:none;');
+    if (content.value !== '') {
+        let todo = {
+            content: content.value,
+            completed: false
         };
-        data.push(todo); //存入data陣列
-        updateList(data); //存入資料後更新網頁內容
-        localStorage.setItem('listData', JSON.stringify(data)); //更新localStorage內容
-        document.querySelector('.search').value = ''; //新增內容後將input清空
-    }
-}
-//更新網頁內容
-function updateList(item) {
-    var str = '';
-    var len = item.length;
-    for (var i = 0; i < len; i++) {
-        str += '<li><label><input type="checkbox"><span>' + item[i].content + '</span></label><a href="#" data-index=' + i + '><i class="fas fa-trash-alt" data-index=' + i + '></i></a></li>';
-    }
-    list.innerHTML = str;
-}
-
-//刪除待辦事項
-function toggleDone(e) {
-    var clickTarget = e.target.nodeName;
-
-    if (clickTarget == 'A' || clickTarget == 'I') {
-        //抓出該事項的data-index，並在localStorage刪除後，更新網頁內容
-        e.preventDefault();
-        var index = e.target.dataset.index;
-        data.splice(index, 1);
-        localStorage.setItem('listData', JSON.stringify(data));
-        updateList(data);
+        data.push(todo);
+        localStorage.setItem('todoList', JSON.stringify(data)); // 更新localStorage
+        content.value = ''; // 點擊新增後，清空input
+        render();
     } else {
-        return;
+        feedback.setAttribute('style', 'display:block;');
     }
+}
+
+// 更新資料
+function updateData(e) {
+    e.preventDefault();
+    let clickTarget = e.target.nodeName;
+
+    let index = e.target.dataset.index;
+    if (clickTarget === 'A') {
+        data.splice(index, 1); // 刪除陣列中的資料
+        localStorage.setItem('todoList', JSON.stringify(data)); // 更新localStorage
+        render(); // 更新畫面
+    } else if (clickTarget === 'LABEL' || clickTarget === 'SPAN' || clickTarget === 'INPUT') {
+        data[index].completed = data[index].completed ? false : true; // 如果是false改為true, true改為false
+        localStorage.setItem('todoList', JSON.stringify(data)); // 更新localStorage
+        render(); // 更新畫面
+    }
+}
+
+// 清除全部
+function clearAll(e) {
+    e.preventDefault();
+    data = [];
+    localStorage.clear();
+    render();
+}
+
+// 處理畫面
+function render() {
+    let str = '';
+    data.forEach(function(item, index) {
+        str += `
+        <li>
+            <label data-index="${index}">
+                <input type="checkbox"  ${item.completed ? 'checked' : ''} data-index="${index}">
+                <span class="list-item ${item.completed ? 'completed' : '' }" data-index="${index}">${item.content}</span>
+            </label>
+            <a href="#" class="close" data-index="${index}">&times;</a>
+        </li>
+        `;
+    })
+    list.innerHTML = str;
 }
